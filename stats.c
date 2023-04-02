6152786861900e9f3669ba36cfb902eaba398d82
Arbab Haider <arbabhaider649@gmail.com>
2023-04-02 14:09:19 +0500

    Adding code implementation in C1M1

--- stats.c
+++ stats.c
@@ -9,19 +9,20 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Statistics and Analysis of Array data
  *
- * <Add Extended Description Here>
+ * Print the statistics and analysis of Array data
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Arbab Haider
+ * @date 02/04/2023
  *
  */
 
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "stats.h"
 
 /* Size of the Data Set */
@@ -36,8 +37,116 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+  
   /* Statistics and Printing Functions Go Here */
+  print_array(test, SIZE);
+  print_statistics(test, SIZE);
 
 }
 
 /* Add other Implementation File Code Here */
+
+void print_statistics(unsigned char data[], int N)
+{
+	unsigned char maxVal  = find_maximum(data, N);	
+	unsigned char minVal  = find_minimum(data, N);
+  	unsigned char avg 	= find_mean(data, N);
+  	unsigned char medVal  = find_median(data, N);
+  	printf("The maximum element in the given array is: %d\r\n",maxVal);
+  	printf("The minimum element in the given array is: %d\r\n",minVal);
+  	printf("The mean of the given array is: %d\r\n",avg);
+  	printf("The median of the given array is: %d\r\n",medVal);
+}
+
+void print_array(unsigned char data[], int N)
+{
+	
+	int i;
+	printf("The given array is: \r\n");
+	for (i = 0; i < N; i++) 
+	{
+		printf("%d ", data[i]);
+	}
+	printf("\r\n");
+}
+
+unsigned char find_median(unsigned char data[], int N) 
+{
+	int i, j, temp;
+    	for (i = 0; i < N - 1; i++) 
+    	{
+        	for (j = 0; j < N - i - 1; j++) 
+        	{
+            		if (data[j] > data[j + 1]) 
+            		{
+                		temp = data[j];
+                		data[j] = data[j + 1];
+                		data[j + 1] = temp;
+            		}
+        	}
+    	}
+	
+    	if (N % 2 != 0) 
+    	{
+        	return data[N/2];
+    	} 
+    	
+    	else 
+    	{
+        	return (data[(N-1)/2] + data[N/2]) / 2.0;
+    	}
+}
+
+unsigned char find_mean(unsigned char data[], int N) 
+{
+	int i;
+	unsigned int sum = 0;
+	
+	for (i=0; i < N; i++)
+	{
+		sum += data[i];
+	}
+	return (unsigned char) (sum / N);
+}
+
+unsigned char find_maximum(unsigned char data[], int N) 
+{
+	int i;
+	unsigned char maxTmp = data[0];
+	for (i=0; i < N; i++)
+	{
+		if (data[i] > maxTmp)
+			maxTmp = data[i];
+	}
+	return maxTmp;
+}
+
+unsigned char find_minimum(unsigned char data[], int N) 
+{
+	int i;
+	unsigned char minTmp = data[0];
+	for (i=0; i < N; i++)
+	{
+		if (data[i] < minTmp)
+			minTmp = data[i];
+	}
+	return minTmp;
+}
+
+void sort_array(unsigned char data[], int N) 
+{
+	int i, j, temp;
+    	for (i = 0; i < N - 1; i++) 
+    	{
+        	for (j = 0; j < N - i - 1; j++) 
+        	{
+            		if (data[j] < data[j + 1]) 
+            		{
+                		temp = data[j];
+                		data[j] = data[j + 1];
+                		data[j + 1] = temp;
+            		}
+        	}
+    	}
+}
+
